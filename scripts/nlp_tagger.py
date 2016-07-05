2c9b124965d8a42334bc6a37ff3bf108dc095d3b
Pedro Lira <pedrohml@gmail.com>
2016-07-05 15:33:21 -0300

    Add option parser to tagger script

--- scripts/nlp_tagger.py
+++ scripts/nlp_tagger.py
@@ -5,12 +5,32 @@ from nlp.tag_cleaner import TagCleaner
 import pkg_resources
 import os, sys
 import pickle
+from optparse import OptionParser
+import codecs
+
+parser = OptionParser()
+parser.set_usage('Usage: nlp_tagger.py [FILE]')
+parser.add_option('-e', '--encoding', help='Set the input encoding (Default to utf-8)', dest='encoding', default='utf-8')
+parser.set_description(
+"""
+FILE\tThe file with body content (If ommited use stdin instead)
+"""
+)
+
+(options, args) = parser.parse_args()
+
+if (len(args) > 0):
+    file_input = open(args[0], 'r')
+else:
+    file_input = sys.stdin
 
 tag_cleaner = TagCleaner()
 
 tag_cleaner.loadModel('tagger')
 
-body_raw = sys.stdin.read()
+body_raw = codecs.getreader(options.encoding)(file_input).read()
+
+file_input.close()
 
 body_final = tag_cleaner.cleanBody(body_raw)
 
