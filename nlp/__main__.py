d5e7933ae866d44dcefc1798626e3a8d101992c3
Pedro Lira <pedrohml@gmail.com>
2016-07-05 15:46:22 -0300

    Include tag cleaner module in nlp main script

--- nlp/__main__.py
+++ nlp/__main__.py
@@ -1,16 +1,28 @@
+from nlp.tag_cleaner import TagCleaner
 from optparse import OptionParser
+import codecs
+import sys
+
 
 parser = OptionParser()
-parser.set_usage('Usage: python -m nlp [options] # WORKING IN PROGRESS')
-# parser.add_option("-f", "--file", dest="filename",
-#                           help="write report to FILE", metavar="FILE")
-# parser.add_option("-q", "--quiet",
-#                           action="store_false", dest="verbose", default=True,
-#                                             help="don't print status messages to stdout")
+parser.set_usage('Usage: python -m nlp [options]')
+parser.add_option('-e', '--encoding', help='Set the input encoding (Default to utf-8)', dest='encoding', default='utf-8')
+parser.add_option("-t", "--tag", action='store_const', const='tag_cleaner', dest='module', help='Set module to tag_cleaner (default)', default='tag_cleaner')
 
 (options, args) = parser.parse_args()
 
-import code;code.interact(local=dict(globals(),**locals()))
+if (len(args) > 0):
+    file_input = open(args[0], 'r')
+else:
+    file_input = sys.stdin
 
-# print options
-# print args
+body_raw = codecs.getreader(options.encoding)(file_input).read()
+
+file_input.close()
+
+if (options.module == 'tag_cleaner'):
+    tag_cleaner = TagCleaner()
+    tag_cleaner.loadModel('tagger')
+    result = tag_cleaner.cleanBody(body_raw)
+
+print result
